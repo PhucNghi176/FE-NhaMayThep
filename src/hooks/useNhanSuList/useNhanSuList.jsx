1227a263aa75e16a7558c4655d2b87de5a3d23ce
Phuc Nghi <meocodien1762003@gmail.com>
2024-05-20 23:40:24 +0700

    .

--- src/hooks/useNhanSuList/useNhanSuList.jsx
+++ src/hooks/useNhanSuList/useNhanSuList.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getListNhanSuSelector } from "../../redux/selector";
-import { filterNhanSu } from "../../redux/slices/nhanSuSlice/nhanSuSlice";
+import { filterNhanSu } from "../../redux/slices/NhanSuSlice/nhanSuSlice";
 
 const useNhanSuList = () => {
   const dispatch = useDispatch();
